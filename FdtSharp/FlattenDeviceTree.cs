0f74675afb2c631af2abb78b50033d5882daaa07
Konrad M. Kruczynski <kkruczynski@antmicro.com>
2014-12-07 11:45:03 +0100

    Reading reservation blocks.

--- FdtSharp/FlattenDeviceTree.cs
+++ FdtSharp/FlattenDeviceTree.cs
@@ -1,16 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FdtSharp
 {
 	public sealed class FlattenDeviceTree
 	{
-		public FlattenDeviceTree(byte[] treeData)
+		public FlattenDeviceTree()
+		{
+			ReservationBlocks = new List<ReservationBlock>();
+		}
+
+		public FlattenDeviceTree(byte[] treeData) : this()
 		{
 			ReadHeader(treeData);
 		}
 
 		public uint Version { get; private set; }
 		public uint LastCompatibleVersion { get; private set; }
+		public uint BootCPUPhysicalId { get; private set; }
+
+		public ICollection<ReservationBlock> ReservationBlocks { get; private set; }
 
 		private void ReadHeader(byte[] treeData)
 		{
@@ -21,6 +30,28 @@ namespace FdtSharp
 			}
 			Version = Utilities.ReadUintBigEndian(treeData, 20);
 			LastCompatibleVersion = Utilities.ReadUintBigEndian(treeData, 24);
+			BootCPUPhysicalId = Utilities.ReadUintBigEndian(treeData, 28);
+
+			// TODO: check for total size vs array size
+			var stringsOffset = (int)Utilities.ReadUintBigEndian(treeData, 12);
+			var stringsSize = (int)Utilities.ReadUintBigEndian(treeData, 32);
+			var stringsData = new ArraySegment<byte>(treeData, stringsOffset, stringsSize);
+
+			var reservationBlockOffset = (int)Utilities.ReadUintBigEndian(treeData, 16);
+			ReadMemoryReservationBlocks(treeData, reservationBlockOffset);
+		}
+
+		private void ReadMemoryReservationBlocks(byte[] treeData, int reservationBlockOffset)
+		{
+			ReservationBlock currentBlock;
+			do
+			{
+				currentBlock = new ReservationBlock(Utilities.ReadUlongBigEndian(treeData, reservationBlockOffset),
+					Utilities.ReadUlongBigEndian(treeData, reservationBlockOffset + 8));
+				reservationBlockOffset += 16;
+				ReservationBlocks.Add(currentBlock);
+			}
+			while(currentBlock != default(ReservationBlock));
 		}
 
 		private const uint Magic = 0xd00dfeed;
--- FdtSharp/Utilities.cs
+++ FdtSharp/Utilities.cs
@@ -9,6 +9,11 @@ namespace FdtSharp
 		{
 			return unchecked((uint)IPAddress.NetworkToHostOrder((int)BitConverter.ToUInt32(array, index)));
 		}
+
+		public static ulong ReadUlongBigEndian(byte[] array, int index)
+		{
+			return unchecked((ulong)IPAddress.NetworkToHostOrder((long)BitConverter.ToUInt64(array, index)));
+		}
 	}
 }
 
